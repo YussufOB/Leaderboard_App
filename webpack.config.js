8bd63651ad36d3f58673ed3782f2bfe3e5988644
YussufOB <hamzyuss1@gmail.com>
2022-05-25 16:36:16 +0100

    Setup linters & webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: {
+    index: path.resolve(__dirname, 'src', 'index.js'),
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html'),
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+
+  mode: 'development',
+};
